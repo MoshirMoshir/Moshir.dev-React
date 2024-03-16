5290a8d6ad01c57b3a48b7467d0813f334768b23
Moshir <alexandermoshir@yahoo.com>
2024-03-16 06:34:46 -0700

    Moved some componenets to their own file

---
+++ my-app/src/components.jsx
@@ -0,0 +1,55 @@
+// components.js
+import React from 'react';
+
+export function NavBar() {
+  return (
+    <nav id="navBar" className="navbar navbar-expand-lg navbar-dark">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="index.html">Moshir.dev</a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link" href="https://github.com/MoshirMoshir/Moshir.dev-React">Source Code</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="games.html">Games</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="piano.html">Piano</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="fireworks.html">Fireworks</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export function Footer() {
+  return (
+    <footer id="footer-section" className="footer" style={{ backgroundColor: "#242434", padding: "20px 0" }}>
+      <div className="container">
+        <div className="row justify-content-center align-items-center">
+          <div className="col-md-12 text-center">
+            <ul className="list-inline">
+              <li className="list-inline-item"><a href="https://discord.com/users/333013440211976193"><i className="bi bi-discord icon"></i></a></li>
+              <li className="list-inline-item"><a href="https://steamcommunity.com/id/moshirmoshir/"><i className="bi bi-steam icon"></i></a></li>
+              <li className="list-inline-item"><a href="https://github.com/MoshirMoshir"><i className="bi bi-github icon"></i></a></li>
+              <li className="list-inline-item"><a href="https://www.youtube.com/channel/UCIt3Oe3qG-pyD7fOiNVgN9g"><i className="bi bi-youtube icon"></i></a></li>
+              <li className="list-inline-item"><a href="https://www.twitch.tv/moshirmoshir"><i className="bi bi-twitch icon"></i></a></li>
+              <li className="list-inline-item"><a href="https://www.reddit.com/user/Moshir_"><i className="bi bi-reddit icon"></i></a></li>
+              <li className="list-inline-item"><a href="https://twitter.com/moshirmoshir"><i className="bi bi-twitter icon"></i></a></li>
+              <li className="list-inline-item"><a href="mailto:moshir@manasys.org"><i className="bi bi-envelope icon"></i></a></li>
+            </ul>
+            <p className="fine-text">©ManaSys.org All rights reserved.</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,37 +1,10 @@
 import React from 'react';
+import { useState } from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import { NavBar, Footer } from './components.jsx';
 import './style.css';
-
-function NavBar() {
-  return (
-    <nav id="navBar" className="navbar navbar-expand-lg navbar-dark">
-      <div className="container-fluid">
-        <a className="navbar-brand" href="index.html">Moshir.dev</a>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <a className="nav-link" href="https://github.com/MoshirMoshir/Moshir.dev-React">Source Code</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="games.html">Games</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="piano.html">Piano</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="fireworks.html">Fireworks</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-  );
-}
 
 function MainSection() {
   return (
@@ -43,7 +16,7 @@ function MainSection() {
             <h1 className="under-construction">Under Construction...</h1>
             <pre>
                                      No Carrots yet,
-              Just React. Much of React.
+              Just React. Quick Reacts.
             </pre>
           </div>
         </div>
@@ -56,40 +29,33 @@ function MainSection() {
 }
 
 function ArrowSection() {
+  const [arrowUp, setArrowUp] = useState(false);
+
+  const handleClick = () => {
+    if (arrowUp) {
+      // If the arrow is currently up, scroll to the top
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+    } else {
+      // If the arrow is currently down, scroll to the footer
+      document.getElementById('footer-section').scrollIntoView({ behavior: 'smooth' });
+    }
+    // Toggle the state of the arrow
+    setArrowUp(!arrowUp);
+  };
+
   return (
     <section id="arrow-section" className="container-fluid" style={{ backgroundColor: "#1c1c2c", padding: "20px 0" }}>
       <div className="row justify-content-center align-items-center">
         <div className="col-md-12 text-center">
-          <a href="#footer-section" className="arrow-link"><i className="bi bi-arrow-down-circle"></i></a>
+          <a href="#" className="arrow-link" onClick={handleClick}>
+            {arrowUp ? <i className="bi bi-arrow-up-circle"></i> : <i className="bi bi-arrow-down-circle"></i>}
+          </a>
         </div>
       </div>
     </section>
   );
 }
 
-function Footer() {
-  return (
-    <footer id="footer-section" className="footer" style={{ backgroundColor: "#242434", padding: "20px 0" }}>
-      <div className="container">
-        <div className="row justify-content-center align-items-center">
-          <div className="col-md-12 text-center">
-            <ul className="list-inline">
-              <li className="list-inline-item"><a href="https://discord.com/users/333013440211976193"><i className="bi bi-discord icon"></i></a></li>
-              <li className="list-inline-item"><a href="https://steamcommunity.com/id/moshirmoshir/"><i className="bi bi-steam icon"></i></a></li>
-              <li className="list-inline-item"><a href="https://github.com/MoshirMoshir"><i className="bi bi-github icon"></i></a></li>
-              <li className="list-inline-item"><a href="https://www.youtube.com/channel/UCIt3Oe3qG-pyD7fOiNVgN9g"><i className="bi bi-youtube icon"></i></a></li>
-              <li className="list-inline-item"><a href="https://www.twitch.tv/moshirmoshir"><i className="bi bi-twitch icon"></i></a></li>
-              <li className="list-inline-item"><a href="https://www.reddit.com/user/Moshir_"><i className="bi bi-reddit icon"></i></a></li>
-              <li className="list-inline-item"><a href="https://twitter.com/moshirmoshir"><i className="bi bi-twitter icon"></i></a></li>
-              <li className="list-inline-item"><a href="mailto:moshir@manasys.org"><i className="bi bi-envelope icon"></i></a></li>
-            </ul>
-            <p className="fine-text">©ManaSys.org All rights reserved.</p>
-          </div>
-        </div>
-      </div>
-    </footer>
-  );
-}
 
 function App() {
   return (
